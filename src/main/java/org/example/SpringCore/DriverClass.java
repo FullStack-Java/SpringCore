ca65093606423bd287704610a1bf75629b0a8927
EllisAC <ellisac@jncb.com>
2021-12-13 10:41:32 -0500

    Removing dependency injection using spring framework. example using the xml configurations and the ApplicationContext

---
+++ src/main/java/org/example/SpringCore/DriverClass.java
@@ -0,0 +1,43 @@
+package org.example.SpringCore;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DriverClass {
+
+    public static void main(String[] args) {
+        /**
+         * We have dependencies on right and left side.
+         * To get rid of the dependencies of the left side, we
+         * introduce the concept of interfaces.
+         */
+        Vehicle Tesla = new Car();
+        Tesla.drive();
+
+        /**
+         * Anytime we use the new keyword,we are hard coding something.
+         * We want to get rid of that dependency by using the spring framework.
+         * So instead of having some like the following:
+         * Vehicle yengyeng = new Bike();
+         * yengyeng.drive();
+         */
+        Vehicle yengyeng = new Bike();
+        yengyeng.drive();
+
+
+        /**
+         * the method get bean comes from two interfaces
+         * BeanFactory
+         * ApplicationContext
+         *
+         * ApplicationContext is a superset of beanfactory, so it is best to use ApplicationContect
+         *
+         * ApplicationContext is an interface so we have tp use classpath... to use it
+         */
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Vehicle bikey = (Vehicle) context.getBean("vehicle");
+        bikey.drive();
+
+    }
+}
